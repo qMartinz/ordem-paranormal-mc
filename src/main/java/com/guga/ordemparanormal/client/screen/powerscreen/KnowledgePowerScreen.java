873f5a884085003e854f84e4d08b4c817219d852
qMartinz <erica.souza123gu@gmail.com>
2022-10-28 21:23:40 -0300

    estrutura "cripta"
    uma dungeon majoritariamente no subsolo (sua entrada é na superficie), onde haverão zumbis de sangue, loot relacionado ao sangue e uma aberração de carne. ainda precisa por o loot, as criaturas e a aberração

--- src/main/java/com/guga/ordemparanormal/client/screen/powerscreen/KnowledgePowerScreen.java
+++ src/main/java/com/guga/ordemparanormal/client/screen/powerscreen/KnowledgePowerScreen.java
@@ -15,5 +15,6 @@ public class KnowledgePowerScreen extends PowerScreen {
         int tabWidth = this.width - 41;
         addPowerIcon(new PowerButton(tabWidth/2 + 27, height/2 - 10, OPPowers.AFINIDADE_CONHECIMENTO));
         addPowerIcon(new PowerButton(tabWidth/2 + 87, height/2 - 10, OPPowers.CONHECIMENTO_AMPLIADO));
+        addPowerIcon(new PowerButton(tabWidth/2 + 147, height/2 - 10, OPPowers.CONHECIMENTO_AMPLIADO_2));
     }
 }
