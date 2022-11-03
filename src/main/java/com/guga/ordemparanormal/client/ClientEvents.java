78ee3033025c11cab0ed39bae52d700c4e16def6
qMartinz <erica.souza123gu@gmail.com>
2022-11-02 22:24:46 -0300

    pequena linha de código
    esqueci dela

--- src/main/java/com/guga/ordemparanormal/client/ClientEvents.java
+++ src/main/java/com/guga/ordemparanormal/client/ClientEvents.java
@@ -1,4 +1,4 @@
-package com.guga.ordemparanormal.common.events;
+package com.guga.ordemparanormal.client;
 
 import com.guga.ordemparanormal.client.screen.AttributeScreen;
 import com.guga.ordemparanormal.common.block.AltarTranscender;
@@ -7,6 +7,7 @@ import com.guga.ordemparanormal.core.OrdemParanormal;
 import net.minecraft.client.Minecraft;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+import net.minecraftforge.eventbus.api.Event;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.LogicalSide;
 import net.minecraftforge.fml.common.Mod;
@@ -16,8 +17,10 @@ public class ClientEvents {
     @SubscribeEvent
     public static void onRightClickBlock(PlayerInteractEvent.RightClickBlock event){
         if (event.getEntityLiving().level.getBlockState(event.getHitVec().getBlockPos()).getBlock() instanceof AltarTranscender){
-            if (event.getSide() == LogicalSide.CLIENT && !(event.getItemStack().getItem() instanceof RitualItem))
+            if (event.getSide() == LogicalSide.CLIENT && !(event.getItemStack().getItem() instanceof RitualItem)) {
                 Minecraft.getInstance().setScreen(new AttributeScreen());
+            }
+            event.setUseItem(Event.Result.DENY);
         }
     }
 }
