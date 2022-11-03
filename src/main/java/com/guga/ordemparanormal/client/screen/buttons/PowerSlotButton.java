04f9a582fd48a014cb3a185e04648972b75f929e
qMartinz <erica.souza123gu@gmail.com>
2022-11-02 21:48:00 -0300

    patch 20221102
    arrumado alguns bugs
    otimização dos atributos
    melhoria no UI
    ajustes pequenos

---
+++ src/main/java/com/guga/ordemparanormal/client/screen/buttons/PowerSlotButton.java
@@ -0,0 +1,48 @@
+package com.guga.ordemparanormal.client.screen.buttons;
+
+import com.guga.ordemparanormal.client.screen.PowerScreen;
+import com.mojang.blaze3d.systems.RenderSystem;
+import com.mojang.blaze3d.vertex.PoseStack;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.components.AbstractButton;
+import net.minecraft.client.gui.narration.NarrationElementOutput;
+import net.minecraft.network.chat.TextComponent;
+
+public class PowerSlotButton extends AbstractButton {
+    private final int slot;
+    public PowerSlotButton(int x, int y, int width, int height, int slot) {
+        super(x, y, width, height, new TextComponent("slot_" + slot));
+        this.slot = slot;
+    }
+
+    @Override
+    public void onPress() {
+        if (Minecraft.getInstance().screen instanceof PowerScreen pScreen){
+            if (pScreen.selectedSlot != slot) {
+                pScreen.selectedSlot = slot;
+            } else pScreen.selectedSlot = 6;
+        }
+    }
+    @Override
+    public void render(PoseStack pPoseStack, int pMouseX, int pMouseY, float pPartialTick) {
+        if (isMouseOver(pMouseX, pMouseY)){
+            RenderSystem.enableBlend();
+            RenderSystem.defaultBlendFunc();
+            RenderSystem.depthMask(false);
+            RenderSystem.disableDepthTest();
+
+            RenderSystem.setShaderColor(1f, 1f, 1f, 0.5f);
+            RenderSystem.setShaderTexture(0, PowerScreen.TEXTURE);
+            blit(pPoseStack, x - 2, y - 4, 0, 134, 26, 30);
+            RenderSystem.setShaderColor(1f, 1f, 1f, 1f);
+
+            RenderSystem.enableDepthTest();
+            RenderSystem.depthMask(true);
+            RenderSystem.disableBlend();
+        }
+    }
+    @Override
+    public void updateNarration(NarrationElementOutput pNarrationElementOutput) {
+
+    }
+}
